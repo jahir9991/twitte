c0847421cc8e860da0572152d8daeff5e3ded0e5
jahir alam <work.jahiralam@gmail.com>
2023-08-06 06:06:25 +0600

    ui improvement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,15 @@
 export const ENV = {
   SESSION_STORAGE_PREFIX:"twitte_",
   API_ENDPOINT: 'https://missingdata.pythonanywhere.com/',
-  // INIT_AUTH:{
-  //   email:'',
-  //   username:'',
-  //   password:''
-  // },
   INIT_AUTH:{
-    email:'jahir@test.com',
-    username:'jahir9991',
-    password:'notsosecurepassword'
-  }
+    email:'',
+    username:'',
+    password:''
+  },
+  // INIT_AUTH:{
+  //   email:'jahir@test.com',
+  //   username:'jahir9991',
+  //   password:'notsosecurepassword'
+  // }
 };
 
