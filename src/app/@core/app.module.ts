822980bb87cb899f1f108e59d1a0f38966008cdc
jahir alam <work.jahiralam@gmail.com>
2023-08-10 17:02:45 +0600

    pwa

--- src/app/@core/app.module.ts
+++ src/app/@core/app.module.ts
@@ -31,12 +31,12 @@ import { PwaBannerComponent } from './pwa/pwa-banner/pwa-banner.component';
       position: 'top-right',
       className: 'toast-glass',
     }),
-    // ServiceWorkerModule.register('ngsw-worker.js', {
-    //   enabled: !isDevMode(),
-    //   // Register the ServiceWorker as soon as the application is stable
-    //   // or after 30 seconds (whichever comes first).
-    //   registrationStrategy: 'registerWhenStable:30000'
-    // }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     {
